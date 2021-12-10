2b9b73c3a90a42715c75266cdd2866ce8bd80a7c
Angela Carraro <angela_carraro@hotmail.it>
2021-12-10 18:03:23 +0100

    Improved code ex2 of day1

--- AdventOfCode2021/day1/ex2.py
+++ AdventOfCode2021/day1/ex2.py
@@ -6,12 +6,11 @@ from utilities.utility import get_input_int
 filename = os.path.join(dir_path, 'input.txt')
 measurements = get_input_int(filename)
 
-sum_three_measurements = []
-for i in range(2, len(measurements)):
-    sum_three_measurements.append(measurements[i] + measurements[i - 1] + measurements[i - 2])
-
 count = 0
-for i in range(1, len(sum_three_measurements)):
-    if sum_three_measurements[i] > sum_three_measurements[i - 1]:
+previous_sum = sum(measurements[:3])
+for i in range(2, len(measurements)):
+    sum_three_measurements = sum(measurements[i - 3:i])
+    if sum_three_measurements > previous_sum:
         count += 1
+    previous_sum = sum_three_measurements
 print(count)
