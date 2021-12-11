5770d9ad8ca2afc691dd8d9a9a31ca5ff0fde5c0
Angela Carraro <angela_carraro@hotmail.it>
2021-12-11 12:28:37 +0100

    Refactoring

--- AdventOfCode2021/day1/ex1.py
+++ AdventOfCode2021/day1/ex1.py
@@ -1,4 +1,5 @@
 import os, sys
+
 dir_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(dir_path, '..'))
 from utilities.utility import get_input_int
@@ -7,6 +8,6 @@ filename = os.path.join(dir_path, 'input.txt')
 measurements = get_input_int(filename)
 count = 0
 for i in range(1, len(measurements)):
-    if measurements[i] > measurements[i-1]:
+    if measurements[i] > measurements[i - 1]:
         count += 1
 print(count)
--- AdventOfCode2021/day1/ex2.py
+++ AdventOfCode2021/day1/ex2.py
@@ -1,4 +1,5 @@
 import os, sys
+
 dir_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(dir_path, '..'))
 from utilities.utility import get_input_int
