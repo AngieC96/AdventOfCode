b5af931a7ae436bbcc1b9307de05d5b90e70f5d8
Angela Carraro <angela_carraro@hotmail.it>
2021-12-11 13:05:38 +0100

    Done ex1 of day2

--- AdventOfCode2021/utilities/utility.py
+++ AdventOfCode2021/utilities/utility.py
@@ -3,3 +3,16 @@ def get_input_int(filename):
         lines = file.readlines()
         lines = [int(line.strip()) for line in lines]
     return lines
+
+def get_input_str(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        lines = [str(line.strip()) for line in lines]
+    return lines
+
+def get_input_str_int(filename):
+    with open(filename) as file:
+        lines = file.readlines()
+        lines = [line.strip().split() for line in lines]
+        lines = [(str(line[0]), int(line[1])) for line in lines]
+    return lines
