594f494ff5a612ec3a910a78f3d58c61f48d7017
Angela Carraro <angela_carraro@hotmail.it>
2021-12-11 13:17:30 +0100

    Done example of ex2 of day2

---
+++ AdventOfCode2021/day2/ex2.py
@@ -0,0 +1,20 @@
+import os, sys
+
+dir_path = os.path.abspath(os.path.dirname(__file__))
+sys.path.append(os.path.join(dir_path, '..'))
+from utilities.utility import get_input_str_int
+
+filename = os.path.join(dir_path, 'example.txt')
+instructions = get_input_str_int(filename)
+
+position, depth, aim = 0, 0, 0
+for instruction in instructions:
+    if instruction[0] == 'forward':
+        position += instruction[1]
+        depth += aim * instruction[1]
+    elif instruction[0] == 'down':
+        aim += instruction[1]
+    elif instruction[0] == 'up':
+        aim -= instruction[1]
+    print(position, depth, aim)
+print(position * depth)
