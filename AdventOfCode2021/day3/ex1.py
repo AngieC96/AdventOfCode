4f05f041c3016c2aeec26cca4c6e0d947950ad17
Angela Carraro <angela_carraro@hotmail.it>
2021-12-12 01:10:07 +0100

    Error in ex1 of day3

--- AdventOfCode2021/day3/ex1.py
+++ AdventOfCode2021/day3/ex1.py
@@ -6,19 +6,20 @@ dir_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(os.path.join(dir_path, '..'))
 from utilities.utility import get_input_str
 
-filename = os.path.join(dir_path, 'example.txt')
+filename = os.path.join(dir_path, 'input.txt')
 report = get_input_str(filename)
 
 l = len(report[0])
-if l > 16:
-    print(f"This method doesn't work if the length of a row ({l}) is greater than 16")
+if len(report) > 16:
+    print(f"This method doesn't work if the number of lines ({len(report)}) is greater than 16")
     exit()
 
+# Sum all the bits column-wise: I know then number of 1's in every column
 sum = 0
 for line in report:
     sum += int(line, 16)
-print("bitwise sum", hex(sum))
 
+# Transform the sum in binary number as requested
 gamma = ''
 for i in hex(sum)[2:]:
     if int(i) > l:
@@ -27,20 +28,11 @@ for i in hex(sum)[2:]:
         gamma += '0'
 
 gamma = int(gamma, 2)
-print("gamma", gamma)
 
-# It is the 1's complement of gamma
-# Flip every bit:
-epsilon = ~gamma
-# Adding 2^l to convert signed to unsigned integer:
-epsilon = epsilon + 2**l # + (1 << l)
-print("epsilon", epsilon, bin(epsilon), int(bin(epsilon), 2))
-
-# It is the 1's complement of gamma
+# Compute the 1's complement of gamma
 # https://www.tutorialspoint.com/find-one-s-complement-of-an-integer-in-cplusplus
 ones = ((1 << l) - 1) # ((2**l) - 1)
-epsilon2 = ones ^ gamma # bitwise xor
-print("epsilon", epsilon2, bin(epsilon2), int(bin(epsilon2), 2))
+epsilon = ones ^ gamma # bitwise xor
 
 power_cons = gamma * epsilon
 print("power_cons", power_cons)
